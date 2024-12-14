af8466261480074d345642f35fd93912aaaea412
xlpmcx <marcograndibnk@gmail.com>
2024-12-14 23:24:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const path = require('path');
 let mainWindow;
 
 function createWindow() {
-  // Crea la finestra principale
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
@@ -19,29 +18,25 @@ function createWindow() {
       enableWebSQL: true,
       experimentalFeatures: true,
     },
-    resizable: true, // La finestra è ridimensionabile
-    frame: true, // Imposta il frame della finestra, quindi i pulsanti di sistema sono visibili
+    resizable: true,
+    frame: true,
   });
 
-  // Carica il sito senza pop-up e annunci
   mainWindow.loadURL('https://streamingcommunity.family');
 
-  // Gestisci la chiusura della finestra
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 
-  // Evita l'apertura di nuove finestre
   mainWindow.webContents.setWindowOpenHandler((details) => {
     return { action: 'deny' };
   });
 
-  // Impedisce il ridimensionamento oltre i limiti
   mainWindow.on('resize', () => {
     const [width, height] = mainWindow.getSize();
     mainWindow.setBounds({
-      width: Math.min(width, 1920), // Limita la larghezza
-      height: Math.min(height, 1080), // Limita l'altezza
+      width: Math.min(width, 1920),
+      height: Math.min(height, 1080),
     });
   });
 }
