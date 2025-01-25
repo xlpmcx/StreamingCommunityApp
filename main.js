c983dd12892bc23be8bd257f2f115fbb17ec0757
xlpmcx <marcograndibnk@gmail.com>
2025-01-25 03:22:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow() {
     frame: true,
   });
 
-  mainWindow.loadURL('https://streamingcommunity.prof');
+  mainWindow.loadURL('https://streamingcommunity.paris');
 
   mainWindow.on('closed', () => {
     mainWindow = null;
