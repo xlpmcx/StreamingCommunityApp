b3b7f655ecbb5742c167113cafa489d041b72ec5
xlpmcx <marcograndibnk@gmail.com>
2025-05-15 15:16:21 +0200

    Update main.js
    update to .wang

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow() {
     frame: true,
   });
 
-  mainWindow.loadURL('https://streamingcommunity.lu');
+  mainWindow.loadURL('https://streamingcommunity.wang');
 
   mainWindow.on('closed', () => {
     mainWindow = null;
